95f1db128206202afc3f0ac58f41efa2b9569675
Royer Carcausto <royer.carcausto@ucsp.edu.pe>
2021-04-12 18:05:11 -0500

    n Primos

---
+++ Royer_Carcausto_CCOMP2-2_Practica _de _Lab _3/Royer_Carcausto_CCOMP2-2_FIBO.cpp
@@ -0,0 +1,23 @@
+//Implemente un programa que imprima los n numeros de la secuencia de fibonacci
+
+#include <iostream>
+using namespace std;
+
+
+int main(){
+	int n;
+	cout<<"ingrese el valor de n: ";
+	cin>>n;
+	int temporal_fibo;
+	int fibo_n1=1;
+	int fibo=0;
+	int i=0;
+	while(n>i){
+		cout<<fibo<<" ";
+		temporal_fibo=fibo;
+		fibo=fibo+fibo_n1;
+		fibo_n1=temporal_fibo;
+		i++;
+	}
+	return 0;
+}
---
+++ Royer_Carcausto_CCOMP2-2_Practica _de _Lab _3/Royer_Carcausto_CCOMP2-2_PERFE.cpp
@@ -0,0 +1,34 @@
+//Implemente un program que imprima los n numeros perfectos.
+
+#include <iostream>
+using namespace std;
+
+
+int main(){
+	int n;
+	cout<<"Ingrese el valor de n: ";
+	cin>>n;
+	
+	int sumaFactores=0;
+	int i=2;
+	while(n>0){
+		for(int j=1;j<i;j++){
+			if(i%j==0){
+				sumaFactores+=j;
+			}
+			if(sumaFactores>i){
+				sumaFactores=0;
+				break;
+			}
+		}
+		if(sumaFactores==i){
+			n--;
+			cout<<i<<" ";
+		}
+		sumaFactores=0;
+		i++;
+	}
+	
+	return 0;
+
+}
